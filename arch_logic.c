3ac5e727d640ad1544383a18dcf106b4ee4d24d8
IlyaChichkov <ilya.chichkov.dev@gmail.com>
2023-02-02 16:59:46 +0300

    Add: make tree

--- arch_logic.c
+++ arch_logic.c
@@ -1,6 +1,7 @@
 #include "types.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 int compareFreq( const void* a, const void* b)
 {
@@ -16,14 +17,31 @@ symbol* sortSymbolsByFreq(symbol* psym, int k){
     qsort( psym, k, sizeof(symbol), compareFreq);
 }
 
-symbol* makeTree(symbol* psym[], int k)     //рeкурсивная функция создания дерева Хафмана. Передаем по указателю значение нового пришедшего элемента. В данном случае, самого нижнего. Также передаем общее количество символов k. Возвращаем - указатель на новую структуру типа symbol
+symbol* makeTree(symbol* psym[], int k)
 {
-    // :)
+    // Рекурсивная функция создания дерева Хаффмана.
+    // Передаем по указателю значение нового пришедшего элемента.
+    // В данном случае, самого нижнего. Также передаем общее количество символов k.
+    // Возвращаем - указатель на новую структуру типа symbol
+
+    printf("\n > k: [%d]", k);
+    if(k == 1) {
+        printf("End");
+        return psym;
+    }
+    symbol *temp = NULL;
+    temp->left = psym[k - 2];
+    temp->right = psym[k - 1];
+    //temp->freq = psym[k - 2]->freq + psym[k - 1]->freq;
+    psym[k - 2] = temp;
+
+    return makeTree(psym, k - 1);
 }
 
 
-void makeCodes(symbol* root)//Рекурсивная функция кодирования. На вход приниает указатель на полученный корень дерева
+void makeCodes(symbol* root)
 {
-   // :)
+    // Рекурсивная функция кодирования.
+    // На вход принимает указатель на полученный корень дерева
 }
 
