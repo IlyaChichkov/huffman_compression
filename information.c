e40b456280467e6fe6e0743cf2061a5488e1aeb7
IlyaChichkov <ilya.chichkov.dev@gmail.com>
2023-02-03 16:38:33 +0300

    Fix: typo

--- information.c
+++ information.c
@@ -2,13 +2,13 @@
 #include "types.h"
 #include "math_func.h"
 
-void set_information(symbol* simbols, int* k, int* kk, int* fsize_2)
+void set_information(symbol* symbols, int* k, int* kk, int* fsize_2)
 {
     for (int i = 0; i < *k; i++)
     {
-        printf("N= %d\tCode= %d\tFreq= %f\tNum=%f\tSymb=%c\n",i, simbols[i].ch, simbols[i].freq, simbols[i].freq*(*kk), simbols[i].ch);
+        printf("N= %d\tCode= %d\tFreq= %f\tNum=%f\tSymb=%c\n", i, symbols[i].ch, symbols[i].freq, symbols[i].freq * (*kk), symbols[i].ch);
     }
     printf("\nLetters = %d\n", *kk);
-    printf("Entropy = %f\n", entropy_count(simbols, k));
+    printf("Entropy = %f\n", entropy_count(symbols, k));
     printf("Bit cost = %f\n", bcost(fsize_2, *kk));
 }
